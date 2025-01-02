a215c1da73f4205472996b3117b74f8160a9e910
Leer002 <164624183+Leer002@users.noreply.github.com>
2025-01-02 19:04:43 +0330

    Update validators.py

--- utils/validators.py
+++ utils/validators.py
@@ -2,29 +2,29 @@ from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _  
 
 class PhoneNumberValidator(RegexValidator):
-    regex = '^98(9[0-3,9]\d{8}|[1-9]\d{9})$'
+    regex = r'^98(9[0-3,9]\d{8}|[1-9]\d{9})$'
     message = 'Phone number must be a VALID 12 digits like 98xxxxxxxxxx'
     code = 'invalid_phone_number'
 
 
 class SKUValidator(RegexValidator):
-    regex = '^[a-zA-Z0-9\_]{6,20}$'
+    regex = r'^[a-zA-Z0-9\_]{6,20}$'
     message = 'SKU must be alphanumeric with 6 to 20 characters'
     code = 'invalid_sku'
 
 class UsernameValidator(RegexValidator):
-    regex = '^[a-zA-Z][a-zA-Z0-9_.\]+$'
+    regex = 'r^[a-zA-Z][a-zA-Z0-9_.\]+$'
     message = _('Enter a valid username starting with a-z. '
                 'This value may contain only letters, numbers and underscore characters.'),
     code = 'invalid_username'
 
 class IDNumberValidator(RegexValidator):
-    regex = '^[0-9]{10}$'
+    regex = r'^[0-9]{10}$'
     message = _('Enter a valid id number.')
     code = 'invalid_id_number'
 
 class BankCardNumberValidator(RegexValidator):
-    regex = '^[0-9]{16}$'
+    regex = r'^[0-9]{16}$'
     message = _('Enter a valid card number.')
     code = 'invalid_bank_card_number'
 
